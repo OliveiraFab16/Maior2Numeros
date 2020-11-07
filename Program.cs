10d6fabfba1a0dff87ba58de70e25c72b0928994
oliveiraFab16 <fabriciocarvalho932@gmail.com>
2020-11-07 00:20:20 -0300

    MelhordeDois

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Maior2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeirovalor;
+            int segundovalor = 0;
+            
+            Console.WriteLine("-----Melhor de Dois-----");
+
+            Console.Write("Digite o Primeiro Valor : ");
+            primeirovalor = Convert.ToInt32(Console.ReadLine());
+
+            if ( primeirovalor > segundovalor )
+            {
+                segundovalor = primeirovalor;
+            } 
+
+            Console.Write("Digite o Segundo Valor: ");
+            segundovalor = Convert.ToInt32(Console.ReadLine());
+
+             if ( primeirovalor > segundovalor )
+             {
+                primeirovalor = segundovalor;
+             }
+             
+            Console.WriteLine("O Maior Numero É: {0}", segundovalor);
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+           
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Maior2Numeros
  Receba dois números. Exiba o maior.
+Ex.:
+
+Digite o primeiro número..: 7
+Digite o segundo número...: 15
+O maior número é 15
